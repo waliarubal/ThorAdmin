9d0d476135555639c0ab1dfff85a345f099fbb51
Rubal Walia <walia.rubal@gmail.com>
2023-12-10 22:58:53 -0800

    Staging changes.

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBase: 'http://localhost:5203/api/v1'
+  apiBase: 'http://brickred.eastus.cloudapp.azure.com:83/api/v1' //'http://localhost:5203/api/v1'
 };
 
 /*
